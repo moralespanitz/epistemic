80558715302e09a06469581d707e93b50957e560
Alexander Morales-Panitz <alexander@moralespanitz.com>
2026-06-03 10:40:59 -0500

    feat: export epistemicExtension factory from @epistemic/omp

--- packages/omp/src/index.ts
+++ packages/omp/src/index.ts
@@ -1,2 +1,3 @@
 export { AMBER_LAB } from "./theme/amber-lab.js";
 export { renderResearchSidebar } from "./layout/ResearchSidebar.js";
+export { default as epistemicExtension } from "./commands/epistemic.js";
